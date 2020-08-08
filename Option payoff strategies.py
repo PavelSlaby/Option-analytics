6fd5508ca0300a04cbe6475a183d2cc9364658da
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2020-08-08 19:27:53 +0200

    Update Option payoff strategies.py

--- Option payoff strategies.py
+++ Option payoff strategies.py
@@ -1,14 +1,134 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  7 18:55:34 2020
+#%% Options profit scenarios
 
-Black Scholes formula
+import pandas as pd
+import matplotlib.pyplot as plt
 
-@author: pavel_000
-"""
+#   s: spot prices
+#   k: strike price
+#   pl: profit/loss
+#   p: option price
+#   be: break-even point
+
+# Long Call
+
+s = list(range(0, 131, 1)) 
+k = 100
+p = 5
+pl = []
+
+for i in s:
+    if i > (k):
+         pl.append(i - p - k) 
+    else:
+        pl.append(-1 * p)  
+    be = k + p
+    
+   
+fig, ax = plt.subplots()
+fig = plt.plot(s, pl, 'b-')
+plt.plot(s, [0] * len(s))
+plt.plot(k, -p, 'r*')
+plt.plot(be, 0, 'g*')
+plt.grid(1)
+ax.set_ylabel('P/L')
+ax.set_xlabel('Spot Price \n \n ' + 'p =' + str(p) + ', k =' + str(k) + ', break-even = ' + str(be) )
+plt.title('P/L of a \'Long Call\'')
 
 
-#  Scenario function
+tab = pd.DataFrame()
+tab['Prices'] = s
+tab['PL'] = pl
+tab = tab.set_index(['Prices'])
+print(tab)  # prints out the table with possible prices/PL
+
+# Long Put
+
+s = list(range(0, 131, 5)) 
+k = 70
+p = 7
+pl = []
+
+for i in s:
+    if i < (k):
+         pl.append(k - i -p) 
+    else:
+        pl.append(-1 * p)  
+   
+   
+fig, ax = plt.subplots()
+fig = plt.plot(s, pl, 'b-')
+plt.plot(s, [0] * len(s))
+plt.plot(k, 0, 'r*')
+ax.set_ylabel('P/L')
+ax.set_xlabel('Spot Price')
+plt.title('P/L of a \'LongPut\'')
+
+tab = pd.DataFrame()
+tab['Prices'] = s
+tab['PL'] = pl
+tab = tab.set_index(['Prices'])
+tab
+
+
+# Short Call
+
+s = list(range(0, 131, 5)) 
+k = 70
+p = 7
+pl = []
+
+for i in s:
+    if i < (k):
+         pl.append(p) 
+    else:
+        pl.append(p + k -i)  
+   
+   
+fig, ax = plt.subplots()
+fig = plt.plot(s, pl, 'b-')
+plt.plot(s, [0] * len(s))
+plt.plot(k, 0, 'r*')
+ax.set_ylabel('P/L')
+ax.set_xlabel('Spot Price')
+plt.title('P/L of a \'ShortCall\'')
+
+tab = pd.DataFrame()
+tab['Prices'] = s
+tab['PL'] = pl
+tab = tab.set_index(['Prices'])
+
+tab
+
+# Short Put
+
+s = list(range(0, 131, 5)) 
+k = 70
+p = 7
+pl = []
+
+for i in s:
+    if i < (k):
+         pl.append(p-k+i) 
+    else:
+        pl.append(p)  
+   
+   
+fig, ax = plt.subplots()
+fig = plt.plot(s, pl, 'b-')
+plt.plot(s, [0] * len(s))
+plt.plot(k, 0, 'r*')
+ax.set_ylabel('P/L')
+ax.set_xlabel('Spot Price')
+plt.title('P/L of a \'ShortPut\'')
+
+tab = pd.DataFrame()
+tab['Prices'] = s
+tab['PL'] = pl
+tab = tab.set_index(['Prices'])
+tab
+
+#%% Scenario function - combines the previsou code to make a function
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -20,6 +140,7 @@ def option_payoff(s, k, p, option = 'LongCall', graph = True, tab = True):
     #   be: break-even point
 
     pl = []
+    s = list(s)
     
     if option == 'LongCall':       
         for i in s:
@@ -72,7 +193,7 @@ def option_payoff(s, k, p, option = 'LongCall', graph = True, tab = True):
         return tab
 
 
-option_payoff(list(range(0, 150)) , 100, 5, 'LongCall', True, True)
+option_payoff(range(0, 150) , 100, 5, 'LongCall', True, True)
 
 #%% Option Strategy Function
 
@@ -131,4 +252,3 @@ Strategy(50, 150,  [[80, 5, 'LongCall'], [100, 10, 'LongPut']])
 Strategy(50, 150,  [[90, 5, 'ShortCall'], [110, 10, 'LongCall'], [80, 5, 'LongPut'], [100, 10, 'ShortPut']], 'Butterfly') # butterfly
 #   Iron condor  (simultaneously holds a bull put spread and a bear call spread)
 Strategy(50, 150,  [[80, 5, 'ShortCall'], [100, 10, 'LongCall'], [90, 5, 'LongPut'], [110, 10, 'ShortPut']], 'Butterfly') # butterfly
-
