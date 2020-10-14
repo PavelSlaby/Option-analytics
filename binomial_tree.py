ad6a987737d50a5c836b6a3d39494a329e64ba28
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2020-10-14 20:57:15 +0200

    Update binomial_tree.py

--- binomial_tree.py
+++ binomial_tree.py
@@ -1,12 +1,16 @@
-# binomial model for pricing european stock options
-
 '''
+# binomial model for pricing european stock options
+# for ilustratory purposes this script generates 3 binomial trees 
+(1st with stock prices, 2nd with intrinsic option values, 3rd with option values using risk neutral posibilities)- 
+even thought this is unnecesarry when using binomila trees to price an option, 
+we could use only one tree to predict the stock prices, and then just simply use a formula to price the option.
+
 S - stock price today
 K - strike price
 T - time until expiration of the option (in time units)
 r - risk-free rate
 sigmna - volatility of the underlying stock
-N - number of steps in the binomial model   
+N - number of steps in the binomial model   (number of final nodes)
 '''    
 
 
@@ -32,19 +36,15 @@ p = (math.exp(r * T/N) - d)/ (u - d) #risk-neutral probability of an up-movement
 PriceTree = np.zeros([N+1, N+1]) #initiates an array of predicted stock prices
 
 
-N #number of final nodes = number of steps
-
-
 for i in range(0, N) :
     PriceTree[i, N] = S0 * (d**i) * u**(N-i)  #generates the stock price at time N (the final node)
 
-
 for j in range(N-1, -1, -1):
     for i in range(0, j + 1):
-       PriceTree[i, j] = (PriceTree[i, j+1]) / u  #we move from the N-1th column back to the first column and by discounting the follwoing column by 1/u
+       PriceTree[i, j] = (PriceTree[i, j+1]) / u  #we move from the N-1th column back to the first column and by discounting the following column by 1/u
     
 # now the tree contains all possible stock prices       
-# lets get the intrinsic value of the option ie. S - K: 
+# lets get the intrinsic value of the option i.e. S - K: 
 
 ValueTree = PriceTree.copy()    
 
@@ -61,18 +61,9 @@ for j in range(N-1, -1, -1):
     for i in range(0, j + 1):      
        FinTree[i, j] =  math.exp(-r * dt)  * (FinTree[i, j+1] * (p) + FinTree[i+1, j+1] * (1-p))
 
-   
 #print(PriceTree)
 #print(ValueTree)
 
 print(FinTree[0,0])
     
 print(FinTree)
-
-
-  
-
-
-
-
-
